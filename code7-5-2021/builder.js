2003e7c01f689208e8bb26e804c4038337a78f7a
w0lfsangel <61670312+w0lfsangel@users.noreply.github.com>
2021-05-07 11:00:40 +0800

    Update builder.js

--- code7-5-2021/builder.js
+++ code7-5-2021/builder.js
@@ -3,11 +3,11 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+        if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.say('🔄 harvest');
         }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+        if(!creep.memory.building && creep.carry.energy == 50) {
             creep.memory.building = true;
             creep.say('🚧 build');
         }
@@ -21,19 +21,20 @@ var roleBuilder = {
             }
         }
         
-        if(creep.carry.energy == creep.carryCapacity) {
+        else {
+            if(creep.carry.energy == creep.carryCapacity) {
             var target_storage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => { return structure.structureType == STRUCTURE_CONTAINER; }});
                 if (creep.transfer(target_storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target_storage, {visualizePathStyle: {stroke: "#ffffff"}});
             }    
-        }
-        
-        else {
+        } 
+            
+            if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+            }}
         }
     }
 };
